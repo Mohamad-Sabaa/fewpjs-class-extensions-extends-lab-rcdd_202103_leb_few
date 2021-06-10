73b302c18c979e4a7f48255252bc5344d45e0fbf
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-06-10 07:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 // Your code here
 
 class Polygon {
-  constructor (side) {
-    this.side = side;
-    this.count = this.side.length;
+  constructor(sides) {
+    this.sides = sides
+    this.count = this.sides.length
   }
+
   get countSides() {
-    return this.side.length;
+    return this.sides.length
   }
-  get perimeter(side) {
-    let count = 0;
-    for (i=0 ; i < side.length ; i++)
-    return this.count;
+
+  get perimeter() {
+    if (!Array.isArray(this.sides)) return;
+    let sum = 0;
+    for (var int of this.sides) {
+      sum += int
+    }
+    return sum
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 3) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid() {
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    let side4 = this.sides[3]
+    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+  }
+
+  get area() {
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    return this.sides[0] * this.sides[0]
   }
 }
