9123049e6657a75379a1e2d3b9fa379a81cb0cce
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-06-10 07:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+class Polygon {
+  constructor (side) {
+    this.side = side;
+    this.count = this.side.length;
+  }
+  get countSides() {
+    return this.side.length;
+  }
+  get perimeter(side) {
+    let count = 0;
+    for (i=0 ; i < side.length ; i++)
+    return this.count;
+  }
+}
